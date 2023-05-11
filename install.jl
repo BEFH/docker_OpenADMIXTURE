09e3367c4a608ca7a9e3e8f41ed3f374973f570b
Brian Fulton-Howard <fultonh1@gmail.com>
2023-05-11 10:18:51 -0400

    fix indent

--- install.jl
+++ install.jl
@@ -1,5 +1,5 @@
-  #!/usr/bin/env julia
-  using Pkg
-  Pkg.add(url="https://github.com/kose-y/SKFR.jl", rev="802fedb")
-  Pkg.add(url="https://github.com/BEFH/OpenADMIXTURE.jl", rev="a60ade5")
-  pkg"add CSV CUDA Tables Printf ArgParse"
+#!/usr/bin/env julia
+using Pkg
+Pkg.add(url="https://github.com/kose-y/SKFR.jl", rev="802fedb")
+Pkg.add(url="https://github.com/BEFH/OpenADMIXTURE.jl", rev="a60ade5")
+pkg"add CSV CUDA Tables Printf ArgParse"
